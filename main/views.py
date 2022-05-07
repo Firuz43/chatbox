61b69e3feeae6e6ab16df1230e3e5804b88abac8
Firuz43 <firuz1199@mail.ru>
2022-05-07 10:42:31 +0400

    register user

--- main/views.py
+++ main/views.py
@@ -37,5 +37,19 @@ def logoutUSer(request):
 def registerUser(request):
     page = 'register'
     form = RegisterUserForm()
+
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(request, username=user.username,
+                                password=request.POST['password1'])
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+
     context = {"form": form}
     return render(request, 'main/login_register.html', context)
